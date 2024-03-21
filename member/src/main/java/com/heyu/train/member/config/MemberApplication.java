7e5325d49265f19e3789b87b5c7ac18076cff992
何宇 <3126905039@qq.com>
2024-03-21 15:19:10 +0800

    新增member模块

---
+++ member/src/main/java/com/heyu/train/member/config/MemberApplication.java
@@ -0,0 +1,18 @@
+package com.heyu.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 功能:
+ * 作者:何宇
+ * 日期：2024/3/21 15:04
+ */
+@SpringBootApplication
+@ComponentScan("com.heyu")
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
