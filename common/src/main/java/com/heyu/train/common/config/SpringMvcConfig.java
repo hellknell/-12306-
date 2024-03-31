be0e096a937188b4d81d0e3d8676d45fce233288
何宇 <3126905039@qq.com>
2024-03-31 21:46:55 +0800

    新增日志拦截器,为了保证其他拦截器中日志打印也有Log_Id值

---
+++ common/src/main/java/com/heyu/train/common/config/SpringMvcConfig.java
@@ -0,0 +1,30 @@
+package com.heyu.train.common.config;
+
+import com.heyu.train.common.interceptor.LogInterceptor;
+import com.heyu.train.common.interceptor.MemberHandleIntercetor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 功能:
+ * 作者:何宇
+ * 日期：2024/3/31 20:09
+ */
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class SpringMvcConfig implements WebMvcConfigurer {
+    final MemberHandleIntercetor memberHandleIntercetor;
+    final LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+        registry.addInterceptor(memberHandleIntercetor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/member/login", "/member/sendCode");
+    }
+}
---
+++ common/src/main/java/com/heyu/train/common/interceptor/LogInterceptor.java
@@ -0,0 +1,22 @@
+package com.heyu.train.common.interceptor;
+
+import cn.hutool.core.util.RandomUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 功能:
+ * 作者:何宇
+ * 日期：2024/3/31 20:48
+ */
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        return true;
+    }
+}
