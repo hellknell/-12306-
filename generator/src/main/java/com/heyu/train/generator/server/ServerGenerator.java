b0c6849e204530ac12f5554867f95b987f6dc438
何宇 <3126905039@qq.com>
2024-04-10 20:43:24 +0800

    集成dom4j,读取generatorConfig.xml,得到tableName和domainObjectName

--- generator/src/main/java/com/heyu/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/heyu/train/generator/server/ServerGenerator.java
@@ -1,33 +1,44 @@
 package com.heyu.train.generator.server;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ServerGenerator {
     //    static boolean readOnly = true;
 //    static String vuePath = "admin/src/views/main/";
 //    static String serverPath = "[module]/src/main/java/com/heyu/train/[module]/";
     static String pomPath = "generator\\pom.xml";
-//    static String module = "";
+
+    //    static String module = "";
 //
 //    static {
 //        new File(serverPath).mkdirs();
 //    }
-
-    public static void main(String[] args) throws Exception {
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
-        HashMap<String, String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<String, String>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
         saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
-        String text = node.getText();
-        System.out.println(text);
+        System.out.println(node.getText());
+        return node.getText();
+    }
+
+    public static void main(String[] args) throws Exception {
         // 获取mybatis-generator
-//        String generatorPath = getGeneratorPath();
+        String generatorPath = getGeneratorPath();
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText()+"\n"+domainObjectName.getText());
+
 //        // 比如generator-config-member.xml，得到module = member
 //        module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
 //        System.out.println("module: " + module);
@@ -105,16 +116,7 @@ public class ServerGenerator {
 //        FreemarkerUtil.generator(fileName, param);
 //    }
 //
-//    private static String getGeneratorPath() throws DocumentException {
-//        SAXReader saxReader = new SAXReader();
-//        Map<String, String> map = new HashMap<String, String>();
-//        map.put("pom", "http://maven.apache.org/POM/4.0.0");
-//        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
-//        Document document = saxReader.read(pomPath);
-//        Node node = document.selectSingleNode("//pom:configurationFile");
-//        System.out.println(node.getText());
-//        return node.getText();
-//    }
+
 //
 //    /**
 //     * 获取所有的Java类型，使用Set去重
