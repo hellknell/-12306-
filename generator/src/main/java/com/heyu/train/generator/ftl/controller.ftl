d650c6cfa915f431019acc4a2fe27391bb79f215
何宇 <3126905039@qq.com>
2024-04-11 19:47:53 +0800

    使用PassengerController控制层模板,生成成功

--- generator/src/main/java/com/heyu/train/generator/ftl/controller.ftl
+++ generator/src/main/java/com/heyu/train/generator/ftl/controller.ftl
@@ -1,11 +1,12 @@
 package com.heyu.train.member.controller;
 
 import com.heyu.train.common.generator.help.PageInfo;
-import com.heyu.train.common.resp.PassengerQueryResp;
+import com.heyu.train.common.resp.${Domain}QueryResp;
 import com.heyu.train.common.resp.Result;
-import com.heyu.train.member.req.PassengerQueryReq;
-import com.heyu.train.member.req.PassengerReq;
-import com.heyu.train.member.service.PassengerSevice;
+import com.heyu.train.member.req.${Domain}QueryReq;
+import com.heyu.train.member.req.${Domain}Req;
+import com.heyu.train.member.service.${Domain}Service;
+import com.heyu.train.member.service.${Domain}Service;
 import context.LoginMemberContext;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -22,29 +23,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Api(tags = "乘客管理")
 @Validated
-@RequestMapping("/passenger")
+@RequestMapping("/${do_main}")
 @RequiredArgsConstructor
-public class PassengerController {
-    final PassengerSevice passengerSevice;
+public class ${Domain}Controller {
+    final ${Domain}Service ${domain}Service;
 
     @ApiOperation(value = "新增/保存乘客")
     @PostMapping("/save")
-    public Result<Void> login(@Valid @RequestBody PassengerReq req) {
+    public Result<Void> login(@Valid @RequestBody ${Domain}Req req) {
         req.setMemberId(LoginMemberContext.getId());
-        passengerSevice.save(req);
+        ${domain}Service.save(req);
         return Result.success();
     }
     @ApiOperation("查询乘客")
     @GetMapping("/queryList")
-    public   Result<PageInfo<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
+    public   Result<PageInfo<${Domain}QueryResp>> queryList(@Valid ${Domain}QueryReq req) {
         req.setMemberId(LoginMemberContext.getId());
-        return Result.success(passengerSevice.queryList(req));
+        return Result.success(${domain}Service.queryList(req));
 
     }
     @ApiOperation("删除乘客")
     @DeleteMapping("/del/{id}")
     public   Result<Void> queryList(@PathVariable  Long id) {
-        passengerSevice.del(id);
+        ${domain}Service.del(id);
         return  Result.success();
 
     }
--- generator/src/main/java/com/heyu/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/heyu/train/generator/server/ServerGenerator.java
@@ -1,13 +1,16 @@
 package com.heyu.train.generator.server;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 import com.heyu.train.generator.util.FreemarkerUtil;
+import freemarker.template.TemplateException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -15,7 +18,7 @@ public class ServerGenerator {
 //    static String vuePath = "admin/src/views/main/";
 //    static String serverPath = "[module]/src/main/java/com/heyu/train/[module]/";
     static String pomPath = "generator\\pom.xml";
-    static String servicePath = "[module]/src/main/java/com/heyu/train/[module]/service/";
+    static String servicePath = "[module]/src/main/java/com/heyu/train/[module]/";
 
     //    static String module = "";
 //
@@ -54,100 +57,18 @@ public class ServerGenerator {
         map.put("domain", domain);
         map.put("Domain", Domain);
         map.put("do_main", do_main);
-        FreemarkerUtil.initConfig("service.ftl");
-        System.out.println(servicePathFinal);
-        FreemarkerUtil.generator(servicePathFinal+Domain+"Service.java", map);
+//        gen(domain, map, "service");
+        gen(servicePathFinal, domain, map, "controller");
+    }
+
+    private static void gen(String serverPath, String domain, HashMap<String, Object> map, String target) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig(target + ".ftl");
+        String dir = serverPath + target+"/";
+        if (!FileUtil.isDirectory(dir)) {
+            FileUtil.mkdir(dir);
+        }
+        System.out.println(serverPath + domain.substring(0, 1).toLowerCase() + domain.substring(1) + target + ".java");
+        FreemarkerUtil.generator(dir + StrUtil.upperFirst(domain) + StrUtil.upperFirst(target) + ".java", map);
         System.out.println("生成成功！！");
-
-        //        // 比如generator-config-member.xml，得到module = member
-//        module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
-//        System.out.println("module: " + module);
-//        serverPath = serverPath.replace("[module]", module);
-//        // new File(servicePath).mkdirs();
-//        System.out.println("servicePath: " + serverPath);
-//
-//        // 读取table节点
-//        Document document = new SAXReader().read("generator/" + generatorPath);
-//        Node table = document.selectSingleNode("//table");
-//        System.out.println(table);
-//        Node tableName = table.selectSingleNode("@tableName");
-//        Node domainObjectName = table.selectSingleNode("@domainObjectName");
-//        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
-//
-//        // 为DbUtil设置数据源
-//        Node connectionURL = document.selectSingleNode("//@connectionURL");
-//        Node userId = document.selectSingleNode("//@userId");
-//        Node password = document.selectSingleNode("//@password");
-//        System.out.println("url: " + connectionURL.getText());
-//        System.out.println("user: " + userId.getText());
-//        System.out.println("password: " + password.getText());
-//        DbUtil.url = connectionURL.getText();
-//        DbUtil.user = userId.getText();
-//        DbUtil.password = password.getText();
-//
-//        // 示例：表名 heyu_test
-//        // Domain = JiawaTest
-//        String Domain = domainObjectName.getText();
-//        // domain = heyuTest
-//        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
-//        // do_main = heyu-test
-//        String do_main = tableName.getText().replaceAll("_", "-");
-//        // 表中文名
-//        String tableNameCn = DbUtil.getTableComment(tableName.getText());
-//        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
-//        Set<String> typeSet = getJavaTypes(fieldList);
-//
-//        // 组装参数
-//        Map<String, Object> param = new HashMap<>();
-//        param.put("module", module);
-//        param.put("Domain", Domain);
-//        param.put("domain", domain);
-//        param.put("do_main", do_main);
-//        param.put("tableNameCn", tableNameCn);
-//        param.put("fieldList", fieldList);
-//        param.put("typeSet", typeSet);
-//        param.put("readOnly", readOnly);
-//        System.out.println("组装参数：" + param);
-//
-//        gen(Domain, param, "service", "service");
-//        gen(Domain, param, "controller/admin", "adminController");
-//        gen(Domain, param, "req", "saveReq");
-//        gen(Domain, param, "req", "queryReq");
-//        gen(Domain, param, "resp", "queryResp");
-//
-//        genVue(do_main, param);
-//    }
-//
-//    private static void gen(String Domain, Map<String, Object> param, String packageName, String target) throws IOException, TemplateException {
-//        FreemarkerUtil.initConfig(target + ".ftl");
-//        String toPath = serverPath + packageName + "/";
-//        new File(toPath).mkdirs();
-//        String Target = target.substring(0, 1).toUpperCase() + target.substring(1);
-//        String fileName = toPath + Domain + Target + ".java";
-//        System.out.println("开始生成：" + fileName);
-//        FreemarkerUtil.generator(fileName, param);
-//    }
-//
-//    private static void genVue(String do_main, Map<String, Object> param) throws IOException, TemplateException {
-//        FreemarkerUtil.initConfig("vue.ftl");
-//        new File(vuePath + module).mkdirs();
-//        String fileName = vuePath + module + "/" + do_main + ".vue";
-//        System.out.println("开始生成：" + fileName);
-//        FreemarkerUtil.generator(fileName, param);
-//    }
-//
-
-//
-//    /**
-//     * 获取所有的Java类型，使用Set去重
-//     */
-//    private static Set<String> getJavaTypes(List<Field> fieldList) {
-//        Set<String> set = new HashSet<>();
-//        for (int i = 0; i < fieldList.size(); i++) {
-//            Field field = fieldList.get(i);
-//            set.add(field.getJavaType());
-//        }
-//        return set;
-//    }
     }
 }
